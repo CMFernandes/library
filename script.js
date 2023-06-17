ffd1cee713f9fea9ebc2ba4d8fd685f62b024e60
cmfernandes <cristianafmfernandes@gmail.com>
2023-06-17 17:33:42 +0100

    submit form event & form validation

--- script.js
+++ script.js
@@ -4,3 +4,54 @@ function Book(title,author,pages,read){
     this.oPages = pages;
     this.oRead = read;
 }
+
+function validateForm(){
+    let isValid = true;
+
+    if (title === ""){
+        isValid = false;
+        title.cl
+    } 
+    if (author === ""){
+        isValid = false; 
+    }
+    if(pages === ""){
+        isValid = false;
+    }
+    return isValid;
+}
+
+let title
+let author;
+let pages;
+let read;
+
+let submitForm = document.getElementById("submitBtn");
+
+submitForm.addEventListener('click', function(event){
+    
+    title = document.getElementById("title").value;
+    author = document.getElementById("author").value;
+    pages = document.getElementById("pages").value;
+
+    if(!validateForm()){
+
+        return 
+    }
+
+    let checkbox = document.getElementById("read");
+    if(checkbox.checked) {
+        read = true;
+    } else {
+        read = false;
+    }
+
+    addBooktoLibrary();
+
+    displayBooks();
+
+    bookForm.style.display = "none";
+    newBookBtn.style.display = "block";
+
+    event.preventDefault();
+})
